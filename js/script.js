09369bde9c02b2a91a59cb84f08987e106a90a4a
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-02-15 18:46:06 +0300

    53

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ window.addEventListener('DOMContentLoaded', ()=> {
         document.body.style.overflow = 'hidden';
         clearInterval(modelTimerId);
     }
-    //const modelTimerId = setTimeout(modalShow, 5000);
+    //const modelTimerId = setTimeout(modalShow, 5000); 
     function modalHide() {
         modal.classList.add('hide');
         modal.classList.remove('show');
@@ -185,4 +185,55 @@ window.addEventListener('DOMContentLoaded', ()=> {
         ".menu .container",
         'menu__item'
     ).render();
+
+    // Forms
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+
+    };
+    
+    function postData(form) {
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
+            const obj = {};
+            formData.forEach((key, value)=>{
+                obj[key] = value;
+            });
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', ()=>{
+                if(request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(()=>{
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
+    forms.forEach(item=>{
+        postData(item);
+    });
+
+
 });
+
